e9e15b4b60feb4118b57899aab1d441b69998bf4
niuniuanran <irene.anran.niu@gmail.com>
2020-07-03 20:50:31 +1200

    Implement user registration

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ app.route("/").get((req, res) => {
     res.render(`./pug/index`, {
         title: "Homepage Home Page Home page",
         message: "Please login",
-        showLogin: true
+        showLogin: true,
+        showRegistration: true
     });
 });
 
@@ -92,6 +93,39 @@ mongo.connect(process.env.DATABASE, (err, client) => {
             req.logout();
             res.redirect("/");
         });
+
+        app.route("/register").post(
+            (req, res, next) => {
+                db.collection("users").findOne(
+                    { username: req.body.username },
+                    function(err, user) {
+                        if (err) {
+                            next(err);
+                        } else if (user) {
+                            res.redirect("/");
+                        } else {
+                            db.collection("users").insertOne(
+                                {
+                                    username: req.body.username,
+                                    password: req.body.password
+                                },
+                                (err, doc) => {
+                                    if (err) {
+                                        res.redirect("/");
+                                    } else {
+                                        next(null, user);
+                                    }
+                                }
+                            );
+                        }
+                    }
+                );
+            },
+            passport.authenticate("local", { failureRedirect: "/" }),
+            (req, res, next) => {
+                res.redirect("/profile");
+            }
+        );
 
         app.use((req, res, next) => {
             res
